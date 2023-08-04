ebb10de6bafe48e11b7b23a93c296be6ee5ffce9
sirisha469 <chsiri469@gmail.com>
2023-08-04 11:13:11 +0530

    rest api files

---
+++ src/main/java/com/example/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,35 @@
+package com.example.restdemo.controller;
+
+import com.example.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return cloudVendor;
+//        return new CloudVendor("c1","Vendor 1","Addresses one", "xxxxxxxxxx");
+    }
+
+    //creating
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    //updating
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
